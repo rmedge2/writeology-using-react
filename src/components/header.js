aa98be509cf51297b1a726082b3e36da19aba5e9
Rodney Edge Jr <rodneyedgejr@gmail.com>
2023-05-16 10:40:45 -0400

    added images

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react";
+import { Component } from "react";
+import React from "react";
+
+
+
+const PageHeader = () =>{
+    return(
+        <div className="header">
+            
+        </div>
+    )
+}
+
+
+export default PageHeader
